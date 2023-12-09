301b80972164c112a5dfd2cf4090aa660c23d41a
Pasindu dineth <pasindudinethpeiris@gmail.com>
2023-12-09 00:01:13 -0600

    feat: loading data from JSON

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -1,21 +1,21 @@
 import * as React from "react"
 import { BsFillExclamationCircleFill } from 'react-icons/bs';
-
+import JSONData from "../data/header.json"
 
 const Header = () => (
     <div className="flex justify-between flex-col items-center md:flex-row">
         <div className="flex justify-between flex-1 z-[5] items-start">
             <div>
             <h1 className="font-heading text-black text-3xl font-semibold text-center xl:font-bold xl:text-left">
-                FINDSOMEREVIEWS
+                {JSONData.website_name}
             </h1>
             <p className="font-body font-medium text-[15px] text-center xl:text-left">
-                Choose Gold Over Junk
+                {JSONData.slogan}
             </p>
             </div>
             <div className="items-center hidden md:flex">
             <BsFillExclamationCircleFill className="mr-2"/>
-            <span className="italic font-medium m-0 p-0">Affiliate Disclosure</span>
+            <span className="italic font-medium m-0 p-0"><a href={JSONData.right_side_link}>{JSONData.right_side_link_name}</a></span>
             </div>
         </div>
     </div>
